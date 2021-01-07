4ca3b4090ab091534902d79e0648af497836ddaf
Arman Dendibaev <43402694+ndoto-dev@users.noreply.github.com>
2021-01-07 19:25:01 +0500

    country page done

--- pages/country/[name].js
+++ pages/country/[name].js
@@ -2,17 +2,37 @@ import Head from "next/head";
 import Footer from "../../components/Footer";
 import cls from "../../styles/Country.module.css";
 
-export default function Country({ flag, name, capital, demonym }) {
+export default function Country({
+  altSpellings,
+  flag,
+  name,
+  capital,
+  languages,
+  demonym,
+  topLevelDomain,
+}) {
   return (
     <>
       <Head>
-        <title>{name}</title>
+        <title>{altSpellings[1]}</title>
       </Head>
       <div className={`container ${cls.country}`}>
         <h1 className={cls.name}>{name}</h1>
         <img src={flag} className={cls.flag} />
+        <div className={cls.languages}>
+          Languages:{" "}
+          {languages.map((lang) => (
+            <p className={cls.lang}>{lang.nativeName}</p>
+          ))}
+        </div>
         <p>Capital: {capital}</p>
         <p>Demonym: {demonym}</p>
+        <div className={cls.domains}>
+          Domain:{" "}
+          {topLevelDomain.map((domain) => (
+            <p className={cls.domain}>{domain}</p>
+          ))}
+        </div>
       </div>
       <Footer />
     </>
